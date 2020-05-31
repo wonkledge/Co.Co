baf18f4d9bf3e6919068ab24c0f639e53fbcc048
Alexandre LEROY <alexandre.wonkledge.leroy@gmail.com>
2020-05-31 12:06:31 +0200

    master: remove handleResponse function on mongoAdapter

--- src/repository/mongoAdapter.js
+++ src/repository/mongoAdapter.js
@@ -6,4 +6,4 @@ const handleData = (data) => resolved(data.map( datum => datum._doc));
 
 const handleError = (error) => rejected(httpResponseWrapper(HTTP_CODE_500, error));
 
-export const handleQueryResponse = (queryResponse) => either(handleData, handleError);
+export const handleQueryResponse = either(handleData, handleError);
