059db15507977805d7b1dfa33a6b5b1b416668a2
Alexandre LEROY <alexandre.wonkledge.leroy@gmail.com>
2020-06-01 00:57:28 +0200

    master: Update Response Handler for mongo request

--- src/repository/mongoAdapter.js
+++ src/repository/mongoAdapter.js
@@ -1,9 +1,9 @@
-import {either, rejected, resolved} from "../promise/promise";
+import {combine, either, rejected, resolved} from "../promise/promise";
 import {HTTP_CODE_500, httpResponseWrapper} from "../httpCode/httpCode";
 
 
-const handleData = (data) => resolved(data.map( datum => datum._doc));
+const handleData = (data) => Array.isArray(data) ? resolved(data.map( datum => datum._doc)) : resolved(data);
 
 const handleError = (error) => rejected(httpResponseWrapper(HTTP_CODE_500, error));
 
-export const handleQueryResponse = either(handleData, handleError);
+export const query = callback => combine(either(handleData, handleError), callback);
